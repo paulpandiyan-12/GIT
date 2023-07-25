b7ff3a09d9f7af66d244a9bccaf32363956ceb71
root <root@ip-172-31-84-24.ec2.internal>
2023-07-25 11:21:09 +0000

    git pratice

---
+++ pythonexamplecode.py
@@ -0,0 +1,8 @@
+my_tuple = ('kamali', 6, 5, 0.97)
+my_list = ['kamali', 6, 5, 0.97]
+print(my_tuple[0])     # output => 'kamali'
+print(my_list[0])     # output => 'kamali'
+my_tuple[0] = 'kamali'    # modifying tuple => throws an error
+my_list[0] = 'kamali'    # modifying list => list modified
+print(my_tuple[0])     # output => 'kamali'
+print(my_list[0])     # output => 'kamali'
